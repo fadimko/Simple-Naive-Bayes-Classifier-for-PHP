03fe3628c42e92d264a49bab9540e5f7bd8e98f5
Alexandr Sorochkin <sorochkin@topface.com>
2015-11-20 13:38:04 +0300

    табуляции заменены пробелами

--- src/SimpleBayesClassifier/Classifier/NaiveBayesClassifierException.php
+++ src/SimpleBayesClassifier/Classifier/NaiveBayesClassifierException.php
@@ -32,72 +32,72 @@ namespace SimpleBayesClassifier\Classifier;
 
 class NaiveBayesClassifierException extends \Exception {
 
-	function __construct($code = NULL, $else = NULL) {
-		switch($code) {
-			case 1001:
-				parent::__construct('Empty configuration array for NaiveBayesClassifier constructor', $code);
-				break;
-			case 1002:
-				parent::__construct('Store must be defined for NaiveBayesClassifier constructor', $code);
-				break;
-			case 1003:
-				parent::__construct('Store Mode must be defined for NaiveBayesClassifier constructor', $code);
-				break;
-			case 1004:
-				parent::__construct('Store DB Credentials must be defined for NaiveBayesClassifier constructor', $code);
-				break;
+    function __construct($code = NULL, $else = NULL) {
+        switch($code) {
+            case 1001:
+                parent::__construct('Empty configuration array for NaiveBayesClassifier constructor', $code);
+                break;
+            case 1002:
+                parent::__construct('Store must be defined for NaiveBayesClassifier constructor', $code);
+                break;
+            case 1003:
+                parent::__construct('Store Mode must be defined for NaiveBayesClassifier constructor', $code);
+                break;
+            case 1004:
+                parent::__construct('Store DB Credentials must be defined for NaiveBayesClassifier constructor', $code);
+                break;
 
-			case 3001:
-				parent::__construct('Empty configuration array for NBCStore', $code);
-				break;
+            case 3001:
+                parent::__construct('Empty configuration array for NBCStore', $code);
+                break;
 
             case 3100:
                 parent::__construct('NaiveBayesClassifierStore needs a storage provided in the constructor', $code);
                 break;
-			case 3101:
-				parent::__construct('NaiveBayesClassifierStoreMySQL needs a database host defined in the constructor', $code);
-				break;
-			case 3102:
-				parent::__construct('NaiveBayesClassifierStoreMySQL needs a database port defined in the constructor', $code);
-				break;
-			case 3103:
-				parent::__construct('NaiveBayesClassifierStoreMySQL needs a database name defined in the constructor', $code);
-				break;
-			case 3104:
-				parent::__construct('NaiveBayesClassifierStoreMySQL needs a database user defined in the constructor', $code);
-				break;
-			case 3105:
-				parent::__construct('NaiveBayesClassifierStoreMySQL needs a database password defined in the constructor', $code);
-				break;
-			case 3106:
-				parent::__construct('Cannot connect to MySQL Host: ' . mysql_error(), $code);
-				break;
-			case 3107:
-				parent::__construct('Cannot connect to MySQL Database', $code);
-				break;
+            case 3101:
+                parent::__construct('NaiveBayesClassifierStoreMySQL needs a database host defined in the constructor', $code);
+                break;
+            case 3102:
+                parent::__construct('NaiveBayesClassifierStoreMySQL needs a database port defined in the constructor', $code);
+                break;
+            case 3103:
+                parent::__construct('NaiveBayesClassifierStoreMySQL needs a database name defined in the constructor', $code);
+                break;
+            case 3104:
+                parent::__construct('NaiveBayesClassifierStoreMySQL needs a database user defined in the constructor', $code);
+                break;
+            case 3105:
+                parent::__construct('NaiveBayesClassifierStoreMySQL needs a database password defined in the constructor', $code);
+                break;
+            case 3106:
+                parent::__construct('Cannot connect to MySQL Host: ' . mysql_error(), $code);
+                break;
+            case 3107:
+                parent::__construct('Cannot connect to MySQL Database', $code);
+                break;
 
-			case 3200:
-				parent::__construct('HandlerSocket Extension for PHP not installed. Refer to http://code.google.com/p/php-handlersocket/ please.', $code);
-				break;
-			case 3201:
-				parent::__construct('Cannot open specified Index', $code);
-				break;
-			case 3202:
-				parent::__construct('NaiveBayesClassifierStoreHandlerSocket needs a database read port defined in the constructor', $code);
-				break;
-			case 3203:
-				parent::__construct('NaiveBayesClassifierStoreHandlerSocket needs a database write port defined in the constructor', $code);
-				break;
-			case 3204:
-				parent::__construct('NaiveBayesClassifierStoreHandlerSocket needs a database name defined in the constructor', $code);
-				break;
-			case 3205:
-				parent::__construct('Opening Index for HandlerSocket failed, Table Select Fields must be an array', $code);
-				break;
+            case 3200:
+                parent::__construct('HandlerSocket Extension for PHP not installed. Refer to http://code.google.com/p/php-handlersocket/ please.', $code);
+                break;
+            case 3201:
+                parent::__construct('Cannot open specified Index', $code);
+                break;
+            case 3202:
+                parent::__construct('NaiveBayesClassifierStoreHandlerSocket needs a database read port defined in the constructor', $code);
+                break;
+            case 3203:
+                parent::__construct('NaiveBayesClassifierStoreHandlerSocket needs a database write port defined in the constructor', $code);
+                break;
+            case 3204:
+                parent::__construct('NaiveBayesClassifierStoreHandlerSocket needs a database name defined in the constructor', $code);
+                break;
+            case 3205:
+                parent::__construct('Opening Index for HandlerSocket failed, Table Select Fields must be an array', $code);
+                break;
 
-			default:
-				parent::__construct('Uncategorized NaiveBayesClassifierException thrown, check Code Trace please');
-		}
-	}
+            default:
+                parent::__construct('Uncategorized NaiveBayesClassifierException thrown, check Code Trace please');
+        }
+    }
 
 }
--- src/SimpleBayesClassifier/Classifier/Store/NaiveBayesClassifierStoreRedis.php
+++ src/SimpleBayesClassifier/Classifier/Store/NaiveBayesClassifierStoreRedis.php
@@ -35,171 +35,171 @@ use SimpleBayesClassifier\Classifier\NaiveBayesClassifierException;
  */
 
 class NaiveBayesClassifierStoreRedis extends NaiveBayesClassifierStore {
-	/** @var ClientInterface $conn */
-	private $conn;
+    /** @var ClientInterface $conn */
+    private $conn;
 
-	private $namespace	= 'nbc-ns';
-	private $blacklist 	= 'nbc-blacklists';
-	private $words 		= "nbc-words";
-	private $sets 		= "nbc-sets";
-	private $cache		= "nbc-cache";
-	public $delimiter	= "_--%%--_";
-	private $wordCount	= "--count--";
+    private $namespace  = 'nbc-ns';
+    private $blacklist  = 'nbc-blacklists';
+    private $words      = "nbc-words";
+    private $sets       = "nbc-sets";
+    private $cache      = "nbc-cache";
+    public $delimiter   = "_--%%--_";
+    private $wordCount  = "--count--";
 
-	function __construct($conf = array()) {
-		if(empty($conf))
-			throw new NaiveBayesClassifierException(3001);
-		if(empty($conf['db_conn'])) {
+    function __construct($conf = array()) {
+        if(empty($conf))
+            throw new NaiveBayesClassifierException(3001);
+        if(empty($conf['db_conn'])) {
             throw new NaiveBayesClassifierException(3100);
-		}
-		if(!empty($conf['namespace']))
-			$this->namespace = $conf['namespace'];
+        }
+        if(!empty($conf['namespace']))
+            $this->namespace = $conf['namespace'];
 
-		// Namespacing
-		$this->blacklist	= "{$this->namespace}-{$this->blacklist}";
-		$this->words		= "{$this->namespace}-{$this->words}";
-		$this->sets			= "{$this->namespace}-{$this->sets}";
-		$this->cache		= "{$this->namespace}-{$this->cache}";
+        // Namespacing
+        $this->blacklist	= "{$this->namespace}-{$this->blacklist}";
+        $this->words		= "{$this->namespace}-{$this->words}";
+        $this->sets			= "{$this->namespace}-{$this->sets}";
+        $this->cache		= "{$this->namespace}-{$this->cache}";
 
-		// Redis connection
+        // Redis connection
         $this->conn = $conf['db_conn'];
-	}
+    }
 
-	public function close() {
-		$this->conn->close();
-	}
+    public function close() {
+        $this->conn->close();
+    }
 
-	/**
-	 * adds a word to blacklist
-	 *
-	 * @param string $word new blacklist word
-	 *
-	 * @return float new score of blacklisted word
-	 */
-	public function addToBlacklist($word) {
-		return $this->conn->hIncrBy($this->blacklist, $word, 1);
-	}
+    /**
+     * adds a word to blacklist
+     *
+     * @param string $word new blacklist word
+     *
+     * @return float new score of blacklisted word
+     */
+    public function addToBlacklist($word) {
+        return $this->conn->hIncrBy($this->blacklist, $word, 1);
+    }
 
-	/**
-	 * removes a word from blacklist
-	 *
-	 * @param string $word blacklisted word to remove
-	 *
-	 * @return int
-	 */
-	public function removeFromBlacklist($word) {
-		return $this->conn->hDel($this->blacklist, $word);
-	}
+    /**
+     * removes a word from blacklist
+     *
+     * @param string $word blacklisted word to remove
+     *
+     * @return int
+     */
+    public function removeFromBlacklist($word) {
+        return $this->conn->hDel($this->blacklist, $word);
+    }
 
-	/**
-	 * is the word already blacklisted
-	 *
-	 * @param string $word word to check
-	 *
-	 * @return bool
-	 */
-	public function isBlacklisted($word) {
-		return $this->conn->hExists($this->blacklist, $word);
-	}
+    /**
+     * is the word already blacklisted
+     *
+     * @param string $word word to check
+     *
+     * @return bool
+     */
+    public function isBlacklisted($word) {
+        return $this->conn->hExists($this->blacklist, $word);
+    }
 
-	public function trainTo($word, $set) {
-		// Words
-		$this->conn->hIncrBy($this->words, $word, 1);
-		$this->conn->hIncrBy($this->words, $this->wordCount, 1);
+    public function trainTo($word, $set) {
+        // Words
+        $this->conn->hIncrBy($this->words, $word, 1);
+        $this->conn->hIncrBy($this->words, $this->wordCount, 1);
 
-		// Sets
-		$key = "{$word}{$this->delimiter}{$set}";
-		$this->conn->hIncrBy($this->words, $key, 1);
-		$this->conn->hIncrBy($this->sets, $set, 1);
-	}
+        // Sets
+        $key = "{$word}{$this->delimiter}{$set}";
+        $this->conn->hIncrBy($this->words, $key, 1);
+        $this->conn->hIncrBy($this->sets, $set, 1);
+    }
 
-	public function deTrainFromSet($word, $set) {
-		$key = "{$word}{$this->delimiter}{$set}";
+    public function deTrainFromSet($word, $set) {
+        $key = "{$word}{$this->delimiter}{$set}";
 
-		$check = $this->conn->hExists($this->words, $word) &&
-			$this->conn->hExists($this->words, $this->wordCount) &&
-			$this->conn->hExists($this->words, $key) &&
-			$this->conn->hExists($this->sets, $set);
+        $check = $this->conn->hExists($this->words, $word) &&
+            $this->conn->hExists($this->words, $this->wordCount) &&
+            $this->conn->hExists($this->words, $key) &&
+            $this->conn->hExists($this->sets, $set);
 
-		if($check) {
-			// Words
-			$this->conn->hIncrBy($this->words, $word, -1);
-			$this->conn->hIncrBy($this->words, $this->wordCount, -1);
+        if($check) {
+            // Words
+            $this->conn->hIncrBy($this->words, $word, -1);
+            $this->conn->hIncrBy($this->words, $this->wordCount, -1);
 
-			// Sets
-			$this->conn->hIncrBy($this->words, $key, -1);
-			$this->conn->hIncrBy($this->sets, $set, -1);
+            // Sets
+            $this->conn->hIncrBy($this->words, $key, -1);
+            $this->conn->hIncrBy($this->sets, $set, -1);
 
-			return TRUE;
-		}
-		else {
-			return FALSE;
-		}
-	}
+            return TRUE;
+        }
+        else {
+            return FALSE;
+        }
+    }
 
-	/**
-	 * Detrain all words from needed set
-	 *
-	 * @param string $word list of words
-	 * @param string $set   needed set
-	 *
-	 * @return bool
-	 */
-	public function deTrainAllFromSet($word, $set) {
-		$key = "{$word}{$this->delimiter}{$set}";
+    /**
+     * Detrain all words from needed set
+     *
+     * @param string $word list of words
+     * @param string $set   needed set
+     *
+     * @return bool
+     */
+    public function deTrainAllFromSet($word, $set) {
+        $key = "{$word}{$this->delimiter}{$set}";
 
-		$check = $this->conn->hExists($this->words, $word) &&
-				$this->conn->hExists($this->words, $this->wordCount) &&
-				$this->conn->hExists($this->words, $key) &&
-				$this->conn->hExists($this->sets, $set);
+        $check = $this->conn->hExists($this->words, $word) &&
+                $this->conn->hExists($this->words, $this->wordCount) &&
+                $this->conn->hExists($this->words, $key) &&
+                $this->conn->hExists($this->sets, $set);
 
-		if($check) {
-			$count = $this->conn->hget($this->words, $key);
+        if($check) {
+            $count = $this->conn->hget($this->words, $key);
 
-			// Words
-			$this->conn->hIncrBy($this->words, $word, -$count);
-			$this->conn->hIncrBy($this->words, $this->wordCount, -$count);
+            // Words
+            $this->conn->hIncrBy($this->words, $word, -$count);
+            $this->conn->hIncrBy($this->words, $this->wordCount, -$count);
 
-			// Sets
-			$this->conn->hIncrBy($this->words, $key, -$count);
-			$this->conn->hIncrBy($this->sets, $set, -$count);
+            // Sets
+            $this->conn->hIncrBy($this->words, $key, -$count);
+            $this->conn->hIncrBy($this->sets, $set, -$count);
 
-			return TRUE;
-		}
-		else {
-			return FALSE;
-		}
-	}
+            return TRUE;
+        }
+        else {
+            return FALSE;
+        }
+    }
 
-	public function getAllSets() {
-		return $this->conn->hKeys($this->sets);
-	}
+    public function getAllSets() {
+        return $this->conn->hKeys($this->sets);
+    }
 
-	public function getSetCount() {
-		return $this->conn->hLen($this->sets);
-	}
+    public function getSetCount() {
+        return $this->conn->hLen($this->sets);
+    }
 
-	public function getWordCount($words) {
+    public function getWordCount($words) {
         return array_combine($words, $this->conn->hMGet($this->words, $words));
-	}
+    }
 
-	public function getAllWordsCount() {
+    public function getAllWordsCount() {
 //		return $this->conn->hGet($this->wordCount, $this->wordCount); // тут скорей всего ошибка, правильно ниже
         return $this->conn->hGet($this->words, $this->wordCount);
-	}
+    }
 
-	public function getSetWordCount($sets) {
+    public function getSetWordCount($sets) {
         return array_combine($sets, $this->conn->hMGet($this->sets, $sets));
-	}
+    }
 
-	public function getWordCountFromSet($words, $sets) {
-		$keys = array();
-		foreach($words as $word) {
-			foreach($sets as $set) {
-				$keys[] = "{$word}{$this->delimiter}{$set}";
-			}
-		}
-		return array_combine($keys, $this->conn->hMGet($this->words, $keys));
-	}
+    public function getWordCountFromSet($words, $sets) {
+        $keys = array();
+        foreach($words as $word) {
+            foreach($sets as $set) {
+                $keys[] = "{$word}{$this->delimiter}{$set}";
+            }
+        }
+        return array_combine($keys, $this->conn->hMGet($this->words, $keys));
+    }
 
 }
